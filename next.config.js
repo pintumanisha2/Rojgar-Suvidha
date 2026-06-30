b0bdd82bc89da0250126e60d058795a01ff14c22
akshatansh <akshatansh02@gmail.com>
2026-07-01 02:18:42 +0530

    fix: add new Supabase hostname to Next.js image remotePatterns so banner images load correctly

--- next.config.js
+++ next.config.js
@@ -13,6 +13,11 @@ const nextConfig = {
         hostname: "dflnrfvngmquaqdtjjhh.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+      {
+        protocol: "https",
+        hostname: "kkfgdzaoukekhlijlfsw.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
     ],
   },
 
